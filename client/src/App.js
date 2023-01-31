f1433433e0ee31bf99e5aee0dc70142f99d44126
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2023-01-31 11:47:04 -0500

    updated index.js and app.js in client/src

---
+++ client/src/App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Home from './pages/Home';
+import Feed from './pages/Feed';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Nav from './components/Nav';
+import { StoreProvider } from './utils/GlobalState';
+import Success from './pages/Success';
+import Profile from './pages/Profile';
+
+const httpLink = createHttpLink({
+    uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <StoreProvider>
+                        <Nav />
+                        <Routes>
+                            <Route
+                                path="/"
+                                element={<Home />}
+                            />
+                            <Route
+                                path="/login"
+                                element={<Login />}
+                            />
+                            <Route
+                                path="/signup"
+                                element={<Signup />}
+                            />
+                            <Route
+                                path="/success"
+                                element={<Success />}
+                            />
+                            <Route
+                                path="/profile"
+                                element={<Profile />}
+                            />
+                            <Route
+                                path="/feed"
+                                element={<Feed />}
+                            />
+                            <Route
+                                path="/shop/"
+                                element={<shop />}
+                            />
+                        </Routes>
+                    </StoreProvider>
+                </div>
+            </Router>
+        </ApolloProvider>
+    );
+}
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from '../images/logos/logo1.png'
+
+const Home = () => {
+  return (
+    <div className="container">
+     <img class="img-fluid" src={logo} alt='logo' />
+    </div>
+  );
+};
+
+export default Home;

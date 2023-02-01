dc1bc473e0a847e1b482abc24ac5961666b89c62
Matrix <rhailu88@gmail.com>
2023-02-01 12:50:01 -0500

    Created query to find many experiences in schemas file.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,13 @@
 const { User, Order, Experiences, Points, Reviews, UserLevel } = require('../models');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+  Query: {
+        //Find Many
+    Experiences: async () => {
+      return await Experiences.find();
+    },
+
+
+

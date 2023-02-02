c249822050707cca844132285005da50c9d0fe85
Matrix <rhailu88@gmail.com>
2023-02-02 02:26:19 -0500

    Added checkout process to return promise in schemas file.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -84,4 +84,40 @@ order: async (parent, { _id }, context) => {
   throw new AuthenticationError('Not logged in');
 },
 
+checkout: async (parent, args, context) => {
+    const url = new URL(context.headers.referer).origin;
+    const order = new Order({ experiences: args.experiences });
+    const line_items = [];
 
+    const { experiences } = await order.populate('experiences');
+
+    for (let i = 0; i < experiences.length; i++) {
+      const experiences = await stripe.experiences.create({
+        name: experiences[i].name,
+        description: experiences[i].description,
+        images: [`${url}/images/${experiences[i].image}`]
+      });
+
+      const price = await stripe.prices.create({
+        experiences: experiences.id,
+        unit_amount: experiences[i].price * 100,
+        currency: 'usd',
+      });
+
+      line_items.push({
+        price: price.id,
+        quantity: 1
+      });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items,
+      mode: 'payment',
+      success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${url}/`
+    });
+
+    return { session: session.id };
+  }
+},

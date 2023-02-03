df64c2945c0b90d594b358bd9575d0f2ad5fa711
Matrix <rhailu88@gmail.com>
2023-02-03 11:05:38 -0500

    Added return for item in cart and submit checkout once logged-in src file.

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -69,3 +69,39 @@ const Cart = () => {
       </div>
     );
   }
+
+  return (
+    <div className="cart">
+      <div className="close" onClick={toggleCart}>
+        [close]
+      </div>
+      <h2>Shopping Cart</h2>
+      {state.cart.length ? (
+        <div>
+          {state.cart.map((item) => (
+            <CartItem key={item._id} item={item} />
+          ))}
+
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+
+            {Auth.loggedIn() ? (
+              <button onClick={submitCheckout}>Checkout</button>
+            ) : (
+              <span>(log in to check out)</span>
+            )}
+          </div>
+        </div>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
+    </div>
+  );
+};
+
+export default Cart;

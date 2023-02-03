ece363e591afaa9ffa4a8a8ead6318aed94053b3
Matrix <rhailu88@gmail.com>
2023-02-03 10:59:02 -0500

    Setup cart for each item in src file.

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -48,3 +48,9 @@ const Cart = () => {
   function submitCheckout() {
     console.log("submitCheckout called")
     const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
